2c89e75c91b16428624b4f8465d6a9f712535b7f
TANAY TANDON <tanay1400089@gmail.com>
2015-08-04 00:43:32 +0530

    Facebook login and G plus login working

--- app/src/main/java/com/contag/app/fragment/LoginFragment.java
+++ app/src/main/java/com/contag/app/fragment/LoginFragment.java
@@ -16,7 +16,6 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
-import com.facebook.GraphRequestAsyncTask;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;

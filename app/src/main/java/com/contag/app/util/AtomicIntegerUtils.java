0e46acc9b3f461ab2b7b296bf1cb39e1363911ef
Tanay Tandon <tanay1400089@gmail.com>
2015-11-20 17:46:05 +0530

    fixed notifications bug

--- app/src/main/java/com/contag/app/util/AtomicIntegerUtils.java
+++ app/src/main/java/com/contag/app/util/AtomicIntegerUtils.java
@@ -1,7 +1,18 @@
 package com.contag.app.util;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by tanaytandon on 20/11/15.
  */
 public class AtomicIntegerUtils {
+
+    private static AtomicInteger mNotificationID;
+
+    public static int getmNotificationID() {
+        if(mNotificationID == null) {
+            mNotificationID = new AtomicInteger(0);
+        }
+        return mNotificationID.incrementAndGet();
+    }
 }

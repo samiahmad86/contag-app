c92ef56b368692b39cd49b2bf5c4a1d3a3186138
rIdDLe <rIdDLe>
2015-11-24 21:52:53 +0530

    changes ui

--- app/src/main/java/com/contag/app/adapter/FeedsAdapter.java
+++ app/src/main/java/com/contag/app/adapter/FeedsAdapter.java
@@ -12,6 +12,11 @@ import com.contag.app.R;
 import com.contag.app.model.FeedsResponse;
 import com.squareup.picasso.Picasso;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class FeedsAdapter extends BaseAdapter {
@@ -51,6 +56,8 @@ public class FeedsAdapter extends BaseAdapter {
             vh.tvFeedsStory = (TextView) convertView.findViewById(R.id.tv_feeds_story);
             vh.tvUsrCuntId = (TextView) convertView.findViewById(R.id.tv_feeds_usr_cunt_id);
             vh.tvUsrName = (TextView) convertView.findViewById(R.id.tv_feeds_usr_name);
+            vh.tvUsrName = (TextView) convertView.findViewById(R.id.tv_feeds_usr_name);
+            vh.tvFeedTime = (TextView) convertView.findViewById(R.id.tv_feeds_createdOn);
             convertView.setTag(vh);
         } else {
             vh = (ViewHolder) convertView.getTag();
@@ -59,7 +66,11 @@ public class FeedsAdapter extends BaseAdapter {
         Picasso.with(mCtxt).load(feed.getAvatarURL()).error(R.drawable.default_profile_pic_small).into(vh.ivUsrProfilePic);
         vh.tvUsrName.setText(feed.name);
         vh.tvUsrCuntId.setText(feed.contagId);
+        String temp=formatTime(feed.createdOn);
+        String temp1=temp.replace("-","/");
+        vh.tvFeedTime.setText(temp1);
         vh.tvFeedsStory.setText(feed.storyText);
+
         return convertView;
     }
 
@@ -67,6 +78,30 @@ public class FeedsAdapter extends BaseAdapter {
         protected TextView tvUsrName;
         protected TextView tvUsrCuntId;
         protected TextView tvFeedsStory;
+        protected TextView tvFeedTime;
         protected ImageView ivUsrProfilePic;
+    }
+    public String formatTime(String time)
+    {
+        String suffix;
+        String temp1= StringUtils.substringBefore(time, "T");
+       // temp1.replace("-","/");
+        String temp2=StringUtils.substringAfter(time, "T");
+        int pos = temp2.indexOf(":");
+        String temp3= StringUtils.substring(temp2, 0, pos + 3);
+
+        String temp=temp1+" "+temp3;
+        DateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        DateFormat outputFormat = new SimpleDateFormat("KK:mm a dd-MM-yy");
+        try {
+            return outputFormat.format(inputFormat.parse(temp));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+       // System.out.println(outputFormat.format(inputFormat.parse(input)));
+        //return temp3+" "+temp1;
+        return "";
+
+
     }
 }
